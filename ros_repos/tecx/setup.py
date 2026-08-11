f66fca0bfa8aa6e9dce1e6fac00c9e632431dc30
TecXCTO <cto.tecx@gmail.com>
2026-08-11 19:04:02 +0530

    Update setup.py

--- ros_repos/tecx/setup.py
+++ ros_repos/tecx/setup.py
@@ -5,7 +5,7 @@ package_name = 'tecx'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=['tecx'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # format: 'executable_name = package_name.file_name:main_function'
-            'talker_node = tecx.tecx_edge:main'
+            'talker_executable = tecx.tecx_edge:main',
+            'talker_node = tecx.my_node:main',
         ],
     },
 )
