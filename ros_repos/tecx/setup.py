870f6358827f02daaedaf3959f7420be4ca261e4
TecXCTO <cto.tecx@gmail.com>
2026-08-11 16:17:23 +0530

    Create setup.py

---
+++ ros_repos/tecx/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_package'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='Custom ROS 2 Python package running on Android Termux',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # format: 'executable_name = package_name.file_name:main_function'
+            'talker_node = my_robot_package.your_script:main'
+        ],
+    },
+)
